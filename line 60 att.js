79c72f913b2e5ff38e4581b75d0f60fe19c28a8c
sbhuju <31581665+sbhuju@users.noreply.github.com>
2018-05-04 14:35:40 -0600

    Update line 60 att.js

--- line 60 att.js
+++ line 60 att.js
@@ -1,1 +1,3 @@
 var attNameData = "<td id ="+ attID + attname + atteid +">" + "<a  href = '" + "/~" + "jewett" + "/ct310/index.php/federation/viewAtt/"  + attID + "/"  + atteid + "/" + "' "  + ">" + "eid: " + atteid + " | " + "attID: " + attID + " | " + "attName: " + attname + "</a>"+"</td>";
+
+Chnage line 60 in att.js to above to make the table look better.(no functonality changed).
