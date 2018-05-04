34d08b2720afc9a7ad52130c83898c7f1933fffb
sbhuju <31581665+sbhuju@users.noreply.github.com>
2018-05-04 14:32:03 -0600

    sanitized input

--- federation.php
+++ federation.php
@@ -1,5 +1,5 @@
-
 <?php
+
 class Controller_Federation extends Controller_Base
 {
 		//home page
@@ -9,6 +9,7 @@ class Controller_Federation extends Controller_Base
 		foreach($demos as $attid){
 		Uri::create('index.php/federation/attraction/'.$attid->id); 
 		}
+
 		// creating attraction image page
 		foreach($demos as $attid){
 			Uri::create('index.php/federation/attrimage/'.$attid->id); 
@@ -19,36 +20,49 @@ class Controller_Federation extends Controller_Base
 	
         $s = $array = array('status' => 'open');
         $jj = Format::forge($s)->to_json();
+
         return $jj;
 	} 
+
 	 public function action_allstatus() {
 	
 	$view = View::forge('federation/allstatus');
-       $this->template->title = 'All Status';
+       $this->template->title = 'All status';
        $this->template->content = $view;
 	} 
+
 	public function action_allattraction() {
 	
 		$view = View::forge('federation/allattraction');
-	   $this->template->title = 'Federation Attractions';
+	   $this->template->title = 'All Attractions';
 	   $this->template->content = $view;
 	} 
+
 	
+
 	public function action_listing() {
 		$demos = Model_Attraction::find("all");
+
 		$arr = array();
+
 		foreach ($demos as $list){
 			array_push($arr,array('id' => $list->id,'name' => $list->title, 'state' => $list->states->name));
 		}
+
 		$stateList = Format::forge($arr)->to_json();
+
 		return $stateList;
 	}
+
 	public function action_attraction($s) {
 		$demos = Model_Attraction::find($s);
 		$arr = array('id' => $demos->id,'name' => $demos->title, 'desc' => $demos->summary, 'state' => $demos->states->name);
 		$attractionDetails = Format::forge($arr)->to_json();
+
 		return $attractionDetails;
+
 	}
+
 	public function action_attrimage($imageid) {
 		$demos = Model_Attraction::find($imageid);
 		$attrImage = $demos->image_url;
@@ -60,13 +74,12 @@ class Controller_Federation extends Controller_Base
 	
 	public function action_viewAtt($id, $eid) {
 		$view = View::forge('federation/viewAtt');
-       $this->template->title = 'Attraction '.$id;
+       $this->template->title = 'Amazing Site';
        $this->template->content = $view;
 		
 		
 		
 	}
-	
 	public function action_shopcart(){
 		$data = array();
 		$view = View::forge('federation/shopcart', $data);
@@ -74,10 +87,17 @@ class Controller_Federation extends Controller_Base
 		$this->template->content = $view;
 
 		if (Input::post('Order')){
+			$Quantity = filter_var(Input::post('Quantity'), FILTER_SANITIZE_STRING);
+			$Brochure = Input::post('Brochure');
+			$Name = filter_var(Input::post('Name'), FILTER_SANITIZE_STRING);
 			$to = Input::post('emailAdd');
+
 			$subject = "Travel Brochure Order";
-			$txt = "You have ". Input::post('Quantity')." "."'".Input::post('Brochure')."'"." brochures coming to"." ".Input::post('Name');
-			$admintext = Input::post('Quantity')." "."'".Input::post('Brochure')."'"." brochures ordered to"." ".Input::post('Name');
+			$txt = "You have ". $Quantity." "."'".$Brochure."'"." brochures coming to"." ".$Name;
+			$admintext = $Quantity." "."'".$Brochure."'"." brochures ordered to"." ".$Name;
+
+		
+			
 			
 		mail($to,$subject,$txt);
 		mail('Aaron.Pereira@colostate.edu',$subject,$admintext);
@@ -89,4 +109,7 @@ class Controller_Federation extends Controller_Base
 		
 	}
 	
+	
+	
+
 }
